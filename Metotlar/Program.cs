073e4908b41257e44fcd74c5fc83293c9c8230b4
Onur VAROL <onurvarol54@gmail.com>
2021-01-16 23:42:51 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Product urun1 = new Product();
+            urun1.Name = "Elma";
+            urun1.Price = 15;
+            urun1.Description = "Amasya elması";
+
+            Product urun2 = new Product();
+            urun2.Name = "Karpuz";
+            urun2.Price = 80;
+            urun2.Description = "Diayarbakır karpuzu";
+
+            Product[] products = new Product[] {urun1,urun2 };
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.Description);
+                Console.WriteLine("-------------------");
+            }
+
+            Console.WriteLine("................Metotlar........");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Kayseri armut", 15, 20);
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Product product)
+        {
+            Console.WriteLine("Tebrikler; Sepete eklendi " + product.Name);
+        }
+
+        public void Ekle2(string Name, string Description, double Price, int StokAdedi)
+        {
+            Console.WriteLine("Tebrikler; Sepete eklendi " + Name);
+        }
+    }
+}
